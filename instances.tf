d2f63507466b5fc9bb40317b4ff2ebe59a859602
Daniel S <dsch87@gmail.com>
2023-11-01 14:46:32 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web-red" {
   ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   subnet_id = aws_subnet.subnet3.id
   vpc_security_group_ids = [aws_security_group.webserver.id]
   //iam_instance_profile = aws_iam_instance_profile.SSMRoleForEC2.name
@@ -13,7 +13,7 @@ resource "aws_instance" "web-red" {
 
 resource "aws_instance" "web-blue" {
   ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   subnet_id = aws_subnet.subnet4.id
   vpc_security_group_ids = [aws_security_group.webserver.id]
   user_data = filebase64("./ec2_blue.sh")
